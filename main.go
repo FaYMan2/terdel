0dcde5b284fb511e61f6e1e981481dfacaa60381
FaYMan2 <suavarnavats244@gmail.com>
2024-12-27 17:06:34 +0530

    feat : added middleware ( logging moved to middleware )

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"os"
 
 	"github.com/FaYMan2/terdel/handlers"
+	"github.com/FaYMan2/terdel/middleware"
 	"github.com/gorilla/mux"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/joho/godotenv"
@@ -31,30 +32,12 @@ func main() {
 	app := &handlers.App{Pool: pool}
 
 	router := mux.NewRouter()
-	router.HandleFunc("/pg-version", logRequest(app.GetPGversionHandler)).Methods(http.MethodGet)
-	router.HandleFunc("/table-names", logRequest(app.GetTableNamesHandler)).Methods(http.MethodGet)
-	router.HandleFunc("/table-schema/{table-name}", logRequest(app.GetTableSchemaHandler)).Methods(http.MethodGet)
-	router.HandleFunc("/constraints", logRequest(app.GetDbConstraintsHandler)).Methods(http.MethodGet)
+	router.Use(middleware.LoggingMiddleware)
+	router.HandleFunc("/pg-version", app.GetPGversionHandler).Methods(http.MethodGet)
+	router.HandleFunc("/table-names", app.GetTableNamesHandler).Methods(http.MethodGet)
+	router.HandleFunc("/table-schema/{table-name}", app.GetTableSchemaHandler).Methods(http.MethodGet)
+	router.HandleFunc("/constraints", app.GetDbConstraintsHandler).Methods(http.MethodGet)
 
 	fmt.Println("Server is running on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", router))
-}
-
-func logRequest(handler http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("Request: %s %s", r.Method, r.URL.Path)
-		responseWriter := &loggingResponseWriter{ResponseWriter: w, statusCode: http.StatusOK}
-		handler(responseWriter, r)
-		log.Printf("Response: %d %s", responseWriter.statusCode, r.URL.Path)
-	}
-}
-
-type loggingResponseWriter struct {
-	http.ResponseWriter
-	statusCode int
-}
-
-func (lrw *loggingResponseWriter) WriteHeader(code int) {
-	lrw.statusCode = code
-	lrw.ResponseWriter.WriteHeader(code)
 }
---
+++ middleware/logging.go
@@ -0,0 +1,25 @@
+package middleware
+
+import (
+	"log"
+	"net/http"
+)
+
+func LoggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Request: %s %s", r.Method, r.URL.Path)
+		responseWriter := &loggingResponseWriter{ResponseWriter: w, statusCode: http.StatusOK}
+		next.ServeHTTP(responseWriter, r)
+		log.Printf("Response: %d %s", responseWriter.statusCode, r.URL.Path)
+	})
+}
+
+type loggingResponseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (lrw *loggingResponseWriter) WriteHeader(code int) {
+	lrw.statusCode = code
+	lrw.ResponseWriter.WriteHeader(code)
+}
